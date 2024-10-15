c3b697445c4574d754d51552c0e5857e2131693d
Julien <julien.epenoy@uha.fr>
2024-10-15 20:50:09 +0200

    calloc correction

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,10 +11,18 @@
 /* ************************************************************************** */
 
 #include<stdlib.h>
+#include "libft.h"
 
-void *ft_calloc(size_t nmemb, size_t size)
-{
-	if(nmemb == 0 || size == 0)
-		return 0;
-	return(malloc(nmemb * size));
+void *ft_calloc(size_t nmemb, size_t size) {
+    size_t total_size;
+	void *ptr;
+
+	total_size = nmemb * size;
+    if (nmemb != 0 && total_size / nmemb != size)
+        return (NULL); 
+    ptr = malloc(total_size);
+    if (!ptr)
+        return (NULL);
+    ft_memset(ptr, 0, total_size);
+    return (ptr);
 }
