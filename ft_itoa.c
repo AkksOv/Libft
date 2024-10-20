cef10f36ac501fa2dc946ba3c511ecead23c3d9c
Julien <julien.epenoy@uha.fr>
2024-10-20 22:59:24 +0200

    Correction itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,11 +12,18 @@
 
 #include<stdlib.h>
 
-static int	count_signs(int n)
+static int	count_signs(long int n)
 {
 	int	i;
 
 	i = 0;
+	if (n == 0)
+		return 1;
+	if (n < 0)
+	{
+		i = 1;
+		n *= -1;
+	}
 	while (n > 0)
 	{
 		n /= 10;
@@ -25,21 +32,29 @@ static int	count_signs(int n)
 	return (i);
 }
 
-
 char	*ft_itoa(int n)
 {
 	int		size;
 	char	*res;
+	long int nb;
 
-	size = count_signs(n);
-	res = malloc(sizeof(char) * size);
+	nb = (long int) n;
+	size = count_signs(nb);
+	res = malloc(sizeof(char) * (size +1));
 	if (res == NULL)
 		return (0);
 	res[size] = '\0';
-	while (--size >= 0)
+	if (nb == 0)
+	    res[0] = '0';
+	if (nb < 0)
 	{
-		res[size] = n % 10 + '0';
-		n /= 10;
+		nb *= -1;
+		res[0] = '-';
+	}
+	while (--size, nb > 0)
+	{
+		res[size] = nb % 10 + '0';
+		nb /= 10;
 	}
 	return (res);
 }
